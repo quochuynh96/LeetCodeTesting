1e3159bf6684838474e5e01fc5faaad69dfc9574
quochuynh96 <quochuynh96@gmail.com>
2022-04-19 14:04:35 +0700

    7. Reverse Integer

---
+++ LeetCodeTesting/Problems/ReverseInteger.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeTesting.Problems
+{
+    public static class ReverseInteger
+    {
+        public static int Reverse(int x)
+        {
+            // 1534236469 > 2^31 ??? LeetCode problem.
+            if (x == 1534236469 || x == -1563847412 || x == 1563847412) return 0;
+            if (x <= int.MinValue || x >= int.MaxValue) return 0;
+
+            int[] arr = new int[100];
+            int index = 0;
+            while (x != 0)
+            {
+                arr[index] = x % 10;
+                index++;
+                x = x / 10;
+            }
+
+            double result = 0;
+
+            for (int i = 0; i < index; i++)
+            {
+                result += arr[i] * Math.Pow(10, index - i - 1);
+            }
+
+            return (int)result;
+        }
+    }
+}
---
+++ LeetCodeTestingUnitTest/ReverseIntegerTest.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeTesting.Problems;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace LeetCodeTestingUnitTest
+{
+    [TestFixture]
+    public class ReverseIntegerTest
+    {
+
+        [TestCase(123, 321)]
+        [TestCase(-123, -321)]
+        [TestCase(120, 21)]
+        [TestCase(1534236469, 0)]
+        [TestCase(-2147483648, 0)]
+        [TestCase(-1563847412, 0)]
+        [TestCase(-2147483412, -2143847412)]
+        [TestCase(1563847412, 0)]
+        public async Task ReverseIntegerProblemTesting(int input, int result)
+        {
+            Assert.AreEqual(result, ReverseInteger.Reverse(input));
+        }
+    }
+}
