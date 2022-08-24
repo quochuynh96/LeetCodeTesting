f58cf7dc3c00a037c2abb115e141bc063ee179ea
quochuynh96 <quochuynh96@gmail.com>
2022-08-24 21:51:53 +0700

    20. Valid Parentheses

---
+++ LeetCodeTesting/Problems/ValidParentheses.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeTesting.Problems
+{
+    public static class ValidParentheses
+    {
+        public static bool Validate(string s)
+        {
+            if (s.Length == 0) return true;
+            if (s.Length == 1) return false;
+
+            string s2 = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+            
+            if (s2.Length == 0 || s2.Contains("()") || s2.Contains("[]") || s2.Contains("{}"))
+            {
+                return Validate(s2);
+            }
+            return false;
+        }
+    }
+}
+
